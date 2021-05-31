675942009961abced3a7664ce70677f4fa9539ac
Khachatur <58373478+KhachaturB@users.noreply.github.com>
2021-05-31 23:27:38 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,10 +4,7 @@ public class StartUI {
 
     public static void main(String[] args) {
         Item item = new Item("Test item");
-        Tracker tracker = new Tracker();
-        tracker.add(item);
 
-        Item foundItem = tracker.findById(item.getId());
-        System.out.println(foundItem.getId() + ": " + foundItem.getName() + " - " + foundItem.getCreated());
+        System.out.println(item);
     }
 }
