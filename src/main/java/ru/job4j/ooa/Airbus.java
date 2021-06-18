971b4a584352ecb0d8e71374a49547a75165296c
Khachatur <58373478+KhachaturB@users.noreply.github.com>
2021-06-19 00:41:42 +0300

    3. Travis CI [#31727]

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -2,7 +2,7 @@ package ru.job4j.ooa;
 
 public final class Airbus extends Aircraft {
 
-    private static final int COUNT_ENGINE = 3;
+    private static final int COUNT_ENGINE = 2;
 
     private String name;
 
