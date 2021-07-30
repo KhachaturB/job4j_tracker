2e92e8c8b215ef46aac24a20ee114da6fa80fefe
Khachatur <58373478+KhachaturB@users.noreply.github.com>
2021-07-30 13:53:17 +0300

    2. Комбинированный компаратор [#10036 #183340]

--- src/test/java/ru/job4j/collection/JobTest.java
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -4,156 +4,108 @@ import org.junit.Assert;
 import org.junit.Test;
 import ru.job4j.collection.Comparators.*;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class JobTest {
 
-    private final Job job1_1 = new Job("Job1", 1);
-    private final Job job1_2 = new Job("Job1", 2);
-    private final Job job1_3 = new Job("Job1", 3);
-    private final Job job2_1 = new Job("Job2", 1);
-    private final Job job2_2 = new Job("Job2", 2);
-    private final Job job2_3 = new Job("Job2", 3);
-    private final Job job3_3 = new Job("Job3", 3);
-    private final Job job4_4 = new Job("Job4", 4);
-    private final Job job5_5 = new Job("Job5", 5);
-
     @Test
     public void nameAscSort() {
-        List<Job> jobs = new ArrayList<>();
-        jobs.add(job3_3);
-        jobs.add(job5_5);
-        jobs.add(job1_1);
-        jobs.add(job2_2);
-        jobs.add(job4_4);
+        Job job1 = new Job("Job1", 1);
+        Job job2 = new Job("Job2", 2);
+        Job job3 = new Job("Job3", 3);
+        Job job4 = new Job("Job4", 4);
+        Job job5 = new Job("Job5", 5);
 
-        Job[] expected = {
-                job1_1,
-                job2_2,
-                job3_3,
-                job4_4,
-                job5_5
-        };
+        List<Job> jobs = Arrays.asList(job3, job5, job1, job2, job4);
+        List<Job> expected = Arrays.asList(job1, job2, job3, job4, job5);
 
         jobs.sort(new JobNameAscComparator());
 
-        Assert.assertArrayEquals(expected, jobs.toArray());
+        Assert.assertEquals(expected, jobs);
     }
 
     @Test
     public void nameDescSort() {
-        List<Job> jobs = new ArrayList<>();
-        jobs.add(job3_3);
-        jobs.add(job5_5);
-        jobs.add(job1_1);
-        jobs.add(job2_2);
-        jobs.add(job4_4);
+        Job job1 = new Job("Job1", 1);
+        Job job2 = new Job("Job2", 2);
+        Job job3 = new Job("Job3", 3);
+        Job job4 = new Job("Job4", 4);
+        Job job5 = new Job("Job5", 5);
 
-        Job[] expected = {
-                job5_5,
-                job4_4,
-                job3_3,
-                job2_2,
-                job1_1
-        };
+        List<Job> jobs = Arrays.asList(job3, job5, job1, job2, job4);
+        List<Job> expected = Arrays.asList(job5, job4, job3, job2, job1);
 
         jobs.sort(new JobNameDescComparator());
 
-        Assert.assertArrayEquals(expected, jobs.toArray());
+        Assert.assertEquals(expected, jobs);
     }
 
     @Test
     public void priorityAscSort() {
-        List<Job> jobs = new ArrayList<>();
-        jobs.add(job3_3);
-        jobs.add(job5_5);
-        jobs.add(job1_1);
-        jobs.add(job2_2);
-        jobs.add(job4_4);
+        Job job1 = new Job("Job1", 1);
+        Job job2 = new Job("Job2", 2);
+        Job job3 = new Job("Job3", 3);
+        Job job4 = new Job("Job4", 4);
+        Job job5 = new Job("Job5", 5);
 
-        Job[] expected = {
-                job1_1,
-                job2_2,
-                job3_3,
-                job4_4,
-                job5_5
-        };
+        List<Job> jobs = Arrays.asList(job3, job5, job1, job2, job4);
+        List<Job> expected = Arrays.asList(job1, job2, job3, job4, job5);
 
         jobs.sort(new JobPriorityAscComparator());
 
-        Assert.assertArrayEquals(expected, jobs.toArray());
+        Assert.assertEquals(expected, jobs);
     }
 
     @Test
     public void priorityDescSort() {
-        List<Job> jobs = new ArrayList<>();
-        jobs.add(job3_3);
-        jobs.add(job5_5);
-        jobs.add(job1_1);
-        jobs.add(job2_2);
-        jobs.add(job4_4);
+        Job job1 = new Job("Job1", 1);
+        Job job2 = new Job("Job2", 2);
+        Job job3 = new Job("Job3", 3);
+        Job job4 = new Job("Job4", 4);
+        Job job5 = new Job("Job5", 5);
 
-        Job[] expected = {
-                job5_5,
-                job4_4,
-                job3_3,
-                job2_2,
-                job1_1
-        };
+        List<Job> jobs = Arrays.asList(job3, job5, job1, job2, job4);
+        List<Job> expected = Arrays.asList(job5, job4, job3, job2, job1);
 
         jobs.sort(new JobPriorityDescComparator());
 
-        Assert.assertArrayEquals(expected, jobs.toArray());
+        Assert.assertEquals(expected, jobs);
     }
 
     @Test
     public void nameAscAndPriorityDescSort() {
-        List<Job> jobs = new ArrayList<>();
-        jobs.add(job2_2);
-        jobs.add(job1_1);
-        jobs.add(job2_1);
-        jobs.add(job1_3);
-        jobs.add(job2_3);
-        jobs.add(job1_2);
+        Job job11 = new Job("Job1", 1);
+        Job job12 = new Job("Job1", 2);
+        Job job13 = new Job("Job1", 3);
+        Job job21 = new Job("Job2", 1);
+        Job job22 = new Job("Job2", 2);
+        Job job23 = new Job("Job2", 3);
 
-        Job[] expected = {
-                job1_3,
-                job1_2,
-                job1_1,
-                job2_3,
-                job2_2,
-                job2_1
-        };
+        List<Job> jobs = Arrays.asList(job22, job11, job21, job13, job23, job12);
+        List<Job> expected = Arrays.asList(job13, job12, job11, job23, job22, job21);
 
         jobs.sort(new JobNameAscComparator()
                 .thenComparing(new JobPriorityDescComparator()));
 
-        Assert.assertArrayEquals(expected, jobs.toArray());
+        Assert.assertEquals(expected, jobs);
     }
 
     @Test
     public void priorityAscAndNameDescSort() {
-        List<Job> jobs = new ArrayList<>();
-        jobs.add(job2_2);
-        jobs.add(job1_1);
-        jobs.add(job2_1);
-        jobs.add(job1_3);
-        jobs.add(job2_3);
-        jobs.add(job1_2);
+        Job job11 = new Job("Job1", 1);
+        Job job12 = new Job("Job1", 2);
+        Job job13 = new Job("Job1", 3);
+        Job job21 = new Job("Job2", 1);
+        Job job22 = new Job("Job2", 2);
+        Job job23 = new Job("Job2", 3);
 
-        Job[] expected = {
-                job2_1,
-                job1_1,
-                job2_2,
-                job1_2,
-                job2_3,
-                job1_3
-        };
+        List<Job> jobs = Arrays.asList(job22, job11, job21, job13, job23, job12);
+        List<Job> expected = Arrays.asList(job21, job11, job22, job12, job23, job13);
 
         jobs.sort(new JobPriorityAscComparator()
                 .thenComparing(new JobNameDescComparator()));
 
-        Assert.assertArrayEquals(expected, jobs.toArray());
+        Assert.assertEquals(expected, jobs);
     }
 }
